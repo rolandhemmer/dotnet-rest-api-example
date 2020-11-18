162273b5ddde7b53906801d446371108e06de93c
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2020-11-18 04:31:27 +0100

    feat: add service configuration object mapping (#3)

---
+++ api/Example.API/Configuration/AppSettings.cs
@@ -0,0 +1,9 @@
+using Example.API.Configuration.Models;
+
+namespace Example.API.Configuration
+{
+    public sealed class AppSettings
+    {
+        public SystemConfigurationModel System { get; set; }
+    }
+}
---
+++ api/Example.API/Configuration/Models/SystemConfigurationModel.cs
@@ -0,0 +1,11 @@
+namespace Example.API.Configuration.Models
+{
+    public sealed class SystemConfigurationModel
+    {
+        public string LogLevel { get; set; }
+
+        public string Name { get; set; }
+
+        public int Port { get; set; }
+    }
+}
