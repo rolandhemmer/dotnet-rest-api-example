609cbf9e76790f2e71642a1f98ed84921a6decf8
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2020-11-18 04:31:28 +0100

    feat: add Serilog and its custom configuration (#11)

--- api/Example.API/Program.cs
+++ api/Example.API/Program.cs
@@ -1,18 +1,51 @@
+using System;
+using Example.API.Configuration.Provider;
+using Example.API.Extensions;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Example.API
 {
     public static class Program
     {
+        #region METHODS
+
         public static IHostBuilder CreateHostBuilder(string[] args)
         {
-            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+            var configurationProvider = new ConfigurationProvider();
+
+            return Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => webBuilder
+                .UseStartup<Startup>()
+                .UseConfiguration(configurationProvider.GetConfiguration())
+                .UseSerilog());
         }
 
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var configurationProvider = new ConfigurationProvider();
+            var system = configurationProvider.AppSettings.System;
+
+            Log.Logger = new LoggerConfiguration()
+                .AddLoggerConfiguration(system.Name, system.LogLevel)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Initializing 'Example API' service");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                Log.Error($"Service unexpectedly terminated: '{exception.Message}'");
+                Log.Debug($"Exception details => {exception}");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
+
+        #endregion METHODS
     }
 }
