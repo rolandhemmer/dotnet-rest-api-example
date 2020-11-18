24e6b7aaf712ee4773fe81ff825e8ea0819d741d
Roland Hemmer <rolandhemmer@users.noreply.github.com>
2020-11-18 08:36:49 +0100

    feat: add "elements" controller layer (#8)

---
+++ api/Example.API/Constants/ControllerConstants.cs
@@ -0,0 +1,12 @@
+namespace Example.API.Constants
+{
+    public static class ControllerConstants
+    {
+        #region ROUTE
+
+        public const string ElementControllerRoute = "elements";
+        public const string ElementControllerRouteTag = "Elements";
+
+        #endregion ROUTE
+    }
+}
